fb98509fa97e1b63b984ee176b8e7c4cb302246f
taniachris8 <dove6464dove@gmail.com>
2025-11-16 15:21:50 +0200

    added faker

--- server.js
+++ server.js
@@ -3,47 +3,35 @@ import cors from "cors";
 import pino from "pino";
 import pinoPretty from "pino-pretty";
 import * as crypto from "crypto";
+import { faker } from "@faker-js/faker";
+
 
 const app = express();
 const logger = pino(pinoPretty());
 
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Content-Type", "application/json");
-  next();
-});
+app.use("/img", express.static("img"));
+
+function createRandomNews() {
+  return {
+    date: faker.date.recent({ days: 10 }),
+    image: faker.image.urlPicsumPhotos({ width: 750, height: 450 }),
+    title: faker.lorem.sentence(),
+  };
+}
 
 app.get("/news", (req, res) => {
-
-    const news = [
-      {
-        id: crypto.randomUUID(),
-        date: Date.now(),
-        image: "./img/1ljx_855v_201217.jpg",
-        title: "Студия A24 объявила о новом триллере с Флоренс Пью",
-      },
-      {
-        id: crypto.randomUUID(),
-        date: Date.now(),
-        image: "./img/srcimggwcn_vgqz_201218.jpg",
-        title: "Том Харди возвращается в образе злодея в новом экшене Netflix",
-      },
-      {
-        id: crypto.randomUUID(),
-        date: Date.now(),
-        image: "./img/srcimgprer_61wi_201217.jpg",
-        title: "Пиксар работает над сиквелом “Души”",
-      },
-    ];
+    const news = faker.helpers.multiple(createRandomNews, {
+      count: 3,
+    });
 
   const responseBody = {
     status: "ok",
-    timestamp: Date.now(),
     news,
   };
 
-  logger.info("New messages requested");
+  logger.info("News requested");
   res.status(200).json(responseBody);
 });
 
